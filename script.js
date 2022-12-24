16dd283ffcf448dd284c0a59c8b1677853af94e4
tanarakchuns <tanarak.chuns@gmail.com>
2022-12-24 15:53:54 +0700

    add resume

--- script.js
+++ script.js
@@ -1,90 +1,10 @@
-// const chatbox = document.querySelector(".terminal-chat");
-// const input = document.querySelector("input");
-// const textArr = [
-//   {
-//     message: "/hello",
-//     response: "Hi, My name is Tanarak!",
-//   },
-//   {
-//     message: "/whatsup",
-//     response: "I always be fine on this static website LOL!",
-//   },
-//   {
-//     message: "/github",
-//     response: `<a target="_blank" href="https://www.github.com/tanachuns">My Github.</a>`,
-//   },
-//   {
-//     message: "/nickname",
-//     response: "You can call me Tana or Ton :D",
-//   },
-//   {
-//     message: "/resources",
-//     response: `I used <a target="_blank" href="https://www.gradient-animator.com">gradient-animator.com</a> to generate cool background.<br>
-//         I used icon from <a target="_blank" href="https://icons8.com/icons/">icons8.</a> <br>
-//         Link to this <a target="_blank" href="https://github.com/Tanachuns/portfolio">Repository</a>`,
-//   },
-//   {
-//     message: "/info",
-//     response: `<p>
-//         ===========================================================<br><br>
-//         <strong>TANARAK CHUNSANIT</strong> aka.TON<br>
-//         Software Engineer.
-//         <br><br>
-//         <u>EXPERIENCES</u> <br>
-//         Soft Square International Co,Ltd | Intern | May 2021 - July 2021.
-//         <br><br>
-//         <u>EDUCATION</u><br>
-//         Rangsit University | Computer Engineering.
-//         <br><br>
-//         <u>CONTACT</u><br>
-//         (+66)81-947-8436<br>
-//         tanarak.chuns@gmail.com
-//         <br><br>
-//         ===========================================================<br>
-//     </p>`,
-//   },
-//   {
-//     message: "/mail",
-//     response: "Say Hi to me on email.",
-//   },
-//   {
-//     message: "/monopoly",
-//     response: "Play monopoly",
-//   },
+const printBtn = document.querySelector("#print");
 
-//   {
-//     message: "/clear",
-//     response: "just refresh this page! this message wont show.",
-//   },
-// ];
+function onPrint() {
+  printBtn.style.display = "none";
+}
 
-// function textBack(reciveMessage) {
-//   let response = "";
-//   for (let i = 0; i < textArr.length; i++) {
-//     switch (reciveMessage.toLowerCase()) {
-//       case "/helpmepapa":
-//         response = response + " " + textArr[i].message + " ";
-//         break;
-
-//       case "/clear":
-//         location.reload();
-//         break;
-
-//       case "/mail":
-//         window.open("mailto:tanarak.chuns@gmail.com", "_blank");
-//         break;
-//       case "/monopoly":
-//         window.open("https://tanachuns.github.io/monopoly/", "_blank");
-//         break;
-//       case textArr[i].message:
-//         response = textArr[i].response;
-//         break;
-//       default:
-//         break;
-//     }
-//   }
-//   return response;
-// }
+printBt.naddEventListener("click", (e) => onPrint());
 
 // function sendMessage(e) {
 //   if (e.key === "Enter") {
@@ -99,4 +19,4 @@
 //   }
 // }
 
-// input.addEventListener("keyup", (e) => sendMessage(e));
+// input.
